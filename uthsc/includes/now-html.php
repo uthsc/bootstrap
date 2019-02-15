8cad1e0f235f1866d23351e430bd8459f88b68b9
Nic Winn <nic.liamg@gmail.com>
2019-02-15 11:08:11 -0600

    moved now items html into an include file

---
+++ uthsc/includes/now-html.php
@@ -0,0 +1,183 @@
+<div class="uthsc-now-loader" style="color: #006a4d; text-align: center;">
+    <span class="fa fa-spinner fa-spin" aria-hidden="true"></span> Loading UTHSC Now
+</div>
+<div class="uthsc-now grid" style="position: relative; height: 1061.33px;">
+    <div class="grid-sizer"></div>
+    <a href="https://www.facebook.com/19251347752/posts/10156270374097753/"
+       class="grid-item uthsc-now--item uthsc-now--image facebook post-01 "
+       style="position: absolute; left: 0%; top: 0px;">
+        <div class="uthsc-now--container">
+            <div class="uthsc-now--image">
+                <img src="https://scontent.xx.fbcdn.net/v/t1.0-0/p180x540/51052027_10156270370962753_6198415450611646464_o.jpg?_nc_cat=101&amp;_nc_ht=scontent.xx&amp;oh=117deb190530e1581b3e2a2d5486fd11&amp;oe=5CFA108F" style="width:100%"></div>
+            <div class="uthsc-now--message"><p>UTHSC College of Medicine leaders and leaders from Methodist Le Bonheur
+                    Healthcare got to take a&nbsp;…</p></div>
+            <div class="uthsc-now--footer">
+                <div class="uthsc-now--service"><span class="fa fa-facebook-official">&nbsp;</span></div>
+                <div class="uthsc-now--date"><p>January 24, 2019</p></div>
+            </div>
+        </div>
+    </a><a href="https://twitter.com/statuses/1088510139886419968"
+           class="grid-item uthsc-now--item uthsc-now--no-image twitter post-02 "
+           style="position: absolute; left: 24.9573%; top: 0px;">
+        <div class="uthsc-now--container">
+            <div class="uthsc-now--message"><p>Memphis #1! For all our students, residents, faculty who moved to Memphis
+                    for UTHSC, show this to&nbsp;…</p></div>
+            <div class="uthsc-now--footer">
+                <div class="uthsc-now--service"><span class="fa fa-twitter">&nbsp;</span></div>
+                <div class="uthsc-now--date"><p>January 24, 2019</p></div>
+            </div>
+        </div>
+    </a><a href="https://www.facebook.com/19251347752/posts/10156266619757753/"
+           class="grid-item uthsc-now--item uthsc-now--image facebook post-03 "
+           style="position: absolute; left: 50%; top: 0px;">
+        <div class="uthsc-now--container">
+            <div class="uthsc-now--image"><img
+                        src="https://scontent.xx.fbcdn.net/v/t1.0-9/s720x720/50589092_10156266618082753_1112607442623004672_o.jpg?_nc_cat=109&amp;_nc_ht=scontent.xx&amp;oh=f2c0e863d0a36f5ee42196c0c41007e5&amp;oe=5D0109B9"
+                        style="width:100%"></div>
+            <div class="uthsc-now--message"><p>The UTHSC chapter of Student National Medical Association held their
+                    annual Dream Big event on&nbsp;…</p></div>
+            <div class="uthsc-now--footer">
+                <div class="uthsc-now--service"><span class="fa fa-facebook-official">&nbsp;</span></div>
+                <div class="uthsc-now--date"><p>January 22, 2019</p></div>
+            </div>
+        </div>
+    </a><a href="https://www.instagram.com/p/Bs5y6Tpglur/"
+           class="grid-item uthsc-now--item uthsc-now--image instagram post-04 "
+           style="position: absolute; left: 74.9573%; top: 0px;">
+        <div class="uthsc-now--container">
+            <div class="uthsc-now--image"><img
+                        src="https://scontent.cdninstagram.com/vp/571ae99ef2afbe77a0caa2702cac78ee/5CFE735D/t51.2885-15/sh0.08/e35/s640x640/47695089_177449713214914_3013704641157602980_n.jpg?_nc_ht=scontent.cdninstagram.com"
+                        style="width:100%"></div>
+            <div class="uthsc-now--message"><p>#mlkday</p></div>
+            <div class="uthsc-now--footer">
+                <div class="uthsc-now--service"><span class="fa fa-instagram">&nbsp;</span></div>
+                <div class="uthsc-now--date"><p>January 21, 2019</p></div>
+            </div>
+        </div>
+    </a><a href="https://www.facebook.com/19251347752/posts/10156257484067753/"
+           class="grid-item uthsc-now--item uthsc-now--image facebook post-05 "
+           style="position: absolute; left: 24.9573%; top: 143px;">
+        <div class="uthsc-now--container">
+            <div class="uthsc-now--image"><img
+                        src="https://scontent.xx.fbcdn.net/v/t1.0-9/q81/s720x720/50277507_10156257481777753_7630427602309611520_o.jpg?_nc_cat=100&amp;_nc_ht=scontent.xx&amp;oh=53d78060a8bd94164b025a48c6eeb8a9&amp;oe=5CC6DB3C"
+                        style="width:100%"></div>
+            <div class="uthsc-now--message"><p>Run. Walk. Crawl. Keep Moving.
+                    #MLK #MLKDay #MLK90 #AmericanHero</p></div>
+            <div class="uthsc-now--footer">
+                <div class="uthsc-now--service"><span class="fa fa-facebook-official">&nbsp;</span></div>
+                <div class="uthsc-now--date"><p>January 21, 2019</p></div>
+            </div>
+        </div>
+    </a><a href="https://twitter.com/statuses/1087333995115831296"
+           class="grid-item uthsc-now--item uthsc-now--image twitter post-06 "
+           style="position: absolute; left: 50%; top: 350px;">
+        <div class="uthsc-now--container">
+            <div class="uthsc-now--image"><img src="https://pbs.twimg.com/media/DxM8htuVsAAWgk3.jpg" style="width:100%">
+            </div>
+            <div class="uthsc-now--message"><p>Run. Walk. Crawl. Keep Moving.
+                    #MLK #MLKDay #MLK90 #AmericanHero https://t.co/Y2uXDlEg8S</p></div>
+            <div class="uthsc-now--footer">
+                <div class="uthsc-now--service"><span class="fa fa-twitter">&nbsp;</span></div>
+                <div class="uthsc-now--date"><p>January 21, 2019</p></div>
+            </div>
+        </div>
+    </a><a href="https://twitter.com/statuses/1086286857321803777"
+           class="grid-item uthsc-now--item uthsc-now--no-image twitter post-07 "
+           style="position: absolute; left: 0%; top: 350px;">
+        <div class="uthsc-now--container">
+            <div class="uthsc-now--message"><p>UTHSC medical students offer help to the homeless, creating Memphis
+                    Street Health, a community&nbsp;…</p></div>
+            <div class="uthsc-now--footer">
+                <div class="uthsc-now--service"><span class="fa fa-twitter">&nbsp;</span></div>
+                <div class="uthsc-now--date"><p>January 18, 2019</p></div>
+            </div>
+        </div>
+    </a><a href="https://twitter.com/statuses/1086386461119782913"
+           class="grid-item uthsc-now--item uthsc-now--no-image twitter post-08 "
+           style="position: absolute; left: 74.9573%; top: 369px;">
+        <div class="uthsc-now--container">
+            <div class="uthsc-now--message"><p>Can’t wait for tomorrow’s Student National Medical Association #DreamBIG
+                    conference!
+
+                    50 Middle&nbsp;…</p></div>
+            <div class="uthsc-now--footer">
+                <div class="uthsc-now--service"><span class="fa fa-twitter">&nbsp;</span></div>
+                <div class="uthsc-now--date"><p>January 18, 2019</p></div>
+            </div>
+        </div>
+    </a><a href="https://twitter.com/statuses/1085947442644828160"
+           class="grid-item uthsc-now--item uthsc-now--no-image twitter post-09 "
+           style="position: absolute; left: 24.9573%; top: 432px;">
+        <div class="uthsc-now--container">
+            <div class="uthsc-now--message"><p>Top 50 Healthcare Simulation Articles of 2018 features @SimGHOSTS 2018
+                    meeting hosted at&nbsp;…</p></div>
+            <div class="uthsc-now--footer">
+                <div class="uthsc-now--service"><span class="fa fa-twitter">&nbsp;</span></div>
+                <div class="uthsc-now--date"><p>January 17, 2019</p></div>
+            </div>
+        </div>
+    </a><a href="https://www.facebook.com/19251347752/posts/10156254044282753/"
+           class="grid-item uthsc-now--item uthsc-now--image facebook post-010 "
+           style="position: absolute; left: 0%; top: 493px;">
+        <div class="uthsc-now--container">
+            <div class="uthsc-now--image"><img
+                        src="https://external.xx.fbcdn.net/safe_image.php?d=AQANfFmwVMFqI2Js&amp;url=https%3A%2F%2Fnews.uthsc.edu%2Fwp-content%2Fuploads%2F2019%2F01%2FDrs.-Weaver-and-Davis-Bariatric-Program-1024x669.jpg&amp;_nc_hash=AQDsmC3iwp2j9-zK"
+                        style="width:100%"></div>
+            <div class="uthsc-now--message"><p>“We are extremely excited to have been able to bring together two
+                    surgeons of such outstanding&nbsp;…</p></div>
+            <div class="uthsc-now--footer">
+                <div class="uthsc-now--service"><span class="fa fa-facebook-official">&nbsp;</span></div>
+                <div class="uthsc-now--date"><p>January 16, 2019</p></div>
+            </div>
+        </div>
+    </a><a href="https://www.instagram.com/p/BstV5rtgXDZ/"
+           class="grid-item uthsc-now--item uthsc-now--image instagram post-011 "
+           style="position: absolute; left: 74.9573%; top: 512px;">
+        <div class="uthsc-now--container">
+            <div class="uthsc-now--image"><img
+                        src="https://scontent.cdninstagram.com/vp/04e801d24a0985ebe0a570956d06d28b/5CF27800/t51.2885-15/sh0.08/e35/s640x640/49652239_375179916380147_4840366185645334827_n.jpg?_nc_ht=scontent.cdninstagram.com"
+                        style="width:100%"></div>
+            <div class="uthsc-now--message"><p>“When it comes to involvement, it’s important to find your passion and
+                    figure out how to use it to&nbsp;…</p></div>
+            <div class="uthsc-now--footer">
+                <div class="uthsc-now--service"><span class="fa fa-instagram">&nbsp;</span></div>
+                <div class="uthsc-now--date"><p>January 16, 2019</p></div>
+            </div>
+        </div>
+    </a><a href="https://twitter.com/statuses/1085625885103931394"
+           class="grid-item uthsc-now--item uthsc-now--no-image twitter post-012 "
+           style="position: absolute; left: 24.9573%; top: 600px;">
+        <div class="uthsc-now--container">
+            <div class="uthsc-now--message"><p>Really proud of the work our students are doing! Meet Hannah Ashitey, one
+                    of our fourth-year&nbsp;…</p></div>
+            <div class="uthsc-now--footer">
+                <div class="uthsc-now--service"><span class="fa fa-twitter">&nbsp;</span></div>
+                <div class="uthsc-now--date"><p>January 16, 2019</p></div>
+            </div>
+        </div>
+    </a><a href="https://twitter.com/statuses/1085280473826410496"
+           class="grid-item uthsc-now--item uthsc-now--no-image twitter post-013 "
+           style="position: absolute; left: 50%; top: 657px;">
+        <div class="uthsc-now--container">
+            <div class="uthsc-now--message"><p>Webb Smith, professor of #pediatrics at UTHSC and exercise physiologist
+                    @lebonheurchild, Fit4Class&nbsp;…</p></div>
+            <div class="uthsc-now--footer">
+                <div class="uthsc-now--service"><span class="fa fa-twitter">&nbsp;</span></div>
+                <div class="uthsc-now--date"><p>January 15, 2019</p></div>
+            </div>
+        </div>
+    </a><a href="https://twitter.com/statuses/1085282701291241473"
+           class="grid-item uthsc-now--item uthsc-now--image twitter post-014 "
+           style="position: absolute; left: 24.9573%; top: 743px;">
+        <div class="uthsc-now--container">
+            <div class="uthsc-now--image"><img src="https://pbs.twimg.com/media/Dw-ygwcWkAI0_l8.jpg" style="width:100%">
+            </div>
+            <div class="uthsc-now--message"><p>.@UTHSCnursing CRNA Concentration Grows as Attractive, Affordable Option
+                    https://t.co/j9zgAsOQcS&nbsp;…</p></div>
+            <div class="uthsc-now--footer">
+                <div class="uthsc-now--service"><span class="fa fa-twitter">&nbsp;</span></div>
+                <div class="uthsc-now--date"><p>January 15, 2019</p></div>
+            </div>
+        </div>
+    </a>
+</div>
